22808051ba311231ed568fb6d26c80e6bcd36b15
Geet Janawalkar <73057446+Gr33nMachine74@users.noreply.github.com>
2022-10-29 19:44:30 +1000

    Added p5.play library and player creation in main

--- engine/player/player.js
+++ engine/player/player.js
@@ -1,3 +1,9 @@
+// Arrow/Space Values
+const LEFT = 37;
+const RIGHT = 39;
+const UP = 38;
+const DOWN = 40;
+const SPACE = 32;
 
 // Player Variables
 let player;
@@ -5,6 +11,8 @@ let p;
 let player_pos;
 let move;
 let speed;
+
+let up, left, down, right;
 
 // Test
 let sc = [100, 200, 0];
@@ -23,12 +31,24 @@ class Player {
 	}
 
 	playerSystem() {
-		
+
 		this.move();
 	}
 
 
 	move() {
+
+		player_pos = p.position;
+
+		if (keyIsDown(UP) == true) up = true;
+		else up = false;
+		if (keyIsDown(DOWN) == true) down = true;
+		else down = false;
+		if (keyIsDown(LEFT) == true) left = true;
+		else left = false;
+		if (keyIsDown(RIGHT) == true) right = true;
+		else right = false;
+
 
 
 		this.playerOutOfBounds();
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+
+
 function setup() {
 	createCanvas(800, 600);
+
+	player = new Player(200, 300, 50, 50);
 }
 
 function draw() {
