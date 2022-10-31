bf59ea8c8600b643f7a3ebb8d517b3f9ccfde281
Geet Janawalkar <73057446+Gr33nMachine74@users.noreply.github.com>
2022-10-31 17:36:06 +1000

    Enemy in position

--- engine/enemy/enemyinit.js
+++ engine/enemy/enemyinit.js
@@ -4,7 +4,7 @@ function enemy_init() {
 
   let enemy_handler = new EnemyHandler();
   
-  lvl1_enemy = new Enemy(0, 0);
+  lvl1_enemy = new Enemy(-600, -100);
   enemy_handler.add(lvl1_enemy);
 
   return enemy_handler;
