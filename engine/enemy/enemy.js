d59c732776233b9ffb3e4f8c6f0a58fe10823fa1
Geet Janawalkar <73057446+Gr33nMachine74@users.noreply.github.com>
2022-10-31 19:58:53 +1000

    Jank but better ground detection for enemies

--- engine/enemy/enemy.js
+++ engine/enemy/enemy.js
@@ -134,20 +134,19 @@ class Enemy {
    * @return {number} the distance between a1 and the interesection
    * if an intersection occurs, infinity if no intersection occurs.
    */
-   line_line_intersect(a1, a2, b1, b2) {
+  line_line_intersect(a1, a2, b1, b2) {
 
-    let d = (a1.x-a2.x)*(b1.y-b2.y) - (a1.y-a2.y)*(b1.x-b2.x);
+    let d = (a1.x - a2.x) * (b1.y - b2.y) - (a1.y - a2.y) * (b1.x - b2.x);
 
-    let t = ((a1.x-b1.x)*(b1.y-b2.y) - (a1.y-b1.y)*(b1.x-b2.x)) / d;
-    let u = ((a1.x-b1.x)*(a1.y-a2.y) - (a1.y-b1.y)*(a1.x-a2.x)) / d;
+    let t = ((a1.x - b1.x) * (b1.y - b2.y) - (a1.y - b1.y) * (b1.x - b2.x)) / d;
+    let u = ((a1.x - b1.x) * (a1.y - a2.y) - (a1.y - b1.y) * (a1.x - a2.x)) / d;
 
-    let intersect = new Vector2(a1.x + t*(a2.x-a1.x), a1.y + t*(a2.y-a1.y));
+    let intersect = new Vector2(a1.x + t * (a2.x - a1.x), a1.y + t * (a2.y - a1.y));
     if (0 <= t && t <= 1 && 0 <= u && u <= 1) {
-      // fill(0, 255, 0)
-      // circle(intersect.x, intersect.y, 10);
+      fill(0, 255, 0)
+      circle(intersect.x, intersect.y, 10);
       return vector2_dist(a1, intersect);
-    }
-    else {
+    } else {
       return Infinity;
     }
   }
@@ -157,10 +156,10 @@ class Enemy {
    */
   raycast(partition) {
 
-    let ray_left = new Vector2(-SCREEN_WIDTH, this.h/2);
+    let ray_left = new Vector2(-SCREEN_WIDTH, this.h / 2);
     ray_left.add(this.pos);
 
-    let ray_right = new Vector2(SCREEN_WIDTH, this.h/2);
+    let ray_right = new Vector2(SCREEN_WIDTH, this.h / 2);
     ray_right.add(this.pos);
 
     let ray_up = new Vector2(0, -SCREEN_HEIGHT);
@@ -169,48 +168,65 @@ class Enemy {
     let ray_down = new Vector2(0, SCREEN_HEIGHT);
     ray_down.add(this.pos);
 
-    // stroke(0);
-    // line(this.pos.x, this.pos.y+this.h/2, ray_left.x, ray_left.y);
-    // line(this.pos.x, this.pos.y+this.h/2, ray_right.x, ray_right.y);
-    // line(this.pos.x, this.pos.y, ray_up.x, ray_up.y);
-    // line(this.pos.x, this.pos.y, ray_down.x, ray_down.y);
-    
+    stroke(0);
+    line(this.pos.x, this.pos.y+this.h/2, ray_left.x, ray_left.y);
+    line(this.pos.x, this.pos.y+this.h/2, ray_right.x, ray_right.y);
+    line(this.pos.x, this.pos.y, ray_up.x, ray_up.y);
+    line(this.pos.x, this.pos.y, ray_down.x, ray_down.y);
+
 
     let dist;
 
     for (let polygon of partition.polygons) {
       for (let edge of polygon.edges) {
-        
+
         dist = this.line_line_intersect(this.pos, ray_up, edge.p1, edge.p2);
-        if (dist < this.h/4) {
-          let overlap = this.h/4 - dist;
-          this.vel.y = (this.vel.y < 0) ? -0.5*this.vel.y : this.vel.y;
-          this.pos.y += overlap/2;
+        if (dist < this.h / 4) {
+          let overlap = this.h / 4 - dist;
+          this.vel.y = (this.vel.y < 0) ? -0.5 * this.vel.y : this.vel.y;
+          this.pos.y += overlap / 2;
         }
-        
+
         dist = this.line_line_intersect(this.pos, ray_down, edge.p1, edge.p2);
         if (dist < this.h) {
           let overlap = this.h - dist;
-          this.vel.y -= this.vel.y/2;
-          this.pos.y -= overlap*1.5;
+          this.vel.y -= this.vel.y / 2;
+          this.pos.y -= overlap * 1.5;
           this.grounded = true;
         }
-        
+
+        /// HERE <><><><><><><><><><><><><>
+        if (this.grounded != true && dist < this.h / 2) {
+          this.dir *= -1;
+          this.vel.x = this.dir * this.speed;
+          this.vel.y = this.vel.y / 2;
+          this.pos.x += this.dir * 500;
+          this.pos.y -= 1000;
+        }
+
         if (vector2_dot(new Vector2(-1, 0), edge.face_normal) < -0.9) {
-          dist = this.line_line_intersect(new Vector2(this.pos.x, this.pos.y+this.h/2), ray_left, edge.p1, edge.p2);
-          if (dist < this.w/2) {
-            let overlap = this.w/2 - dist;
-            this.vel.x -= this.vel.x/2;
-            this.pos.x += overlap/2;
+          dist = this.line_line_intersect(new Vector2(this.pos.x, this.pos.y + this.h / 2), ray_left, edge.p1, edge.p2);
+          if (dist < this.w / 2) {
+            let overlap = this.w / 2 - dist;
+            this.vel.x -= this.vel.x / 2;
+            this.pos.x += overlap / 2;
+            if (dist <= 20 && this.grounded == true) {
+              this.dir *= -1;
+              this.pos.x += this.dir * 30;
+            }
           }
         }
-        
+
         if (vector2_dot(new Vector2(+1, 0), edge.face_normal) < -0.9) {
-          dist = this.line_line_intersect(new Vector2(this.pos.x, this.pos.y+this.h/2), ray_right, edge.p1, edge.p2);
-          if (dist < this.w/2) {
-            let overlap = this.w/2 - dist;
-            this.vel.x -= this.vel.x/2;
-            this.pos.x -= overlap/2;
+          dist = this.line_line_intersect(new Vector2(this.pos.x, this.pos.y + this.h / 2), ray_right, edge.p1, edge.p2);
+          if (dist < this.w / 2) {
+            let overlap = this.w / 2 - dist;
+            this.vel.x -= this.vel.x / 2;
+            this.pos.x -= overlap / 2;
+            if (dist <= 20 && this.grounded == true) {
+              this.dir *= -1;
+              this.pos.x += this.dir * 30;
+            }
           }
         }
       }
