60df2e24f59a8e2c9b8ddbc0fac6f81f41b27412
Michael Ellicott <mellic03@gmail.com>
2022-11-07 09:41:37 +1000

    Map directory restructure

--- engine/map/mapinit.js
+++ engine/map/mapinit.js
@@ -2,7 +2,7 @@ function map_init() {
 
   let map_handler = new MapHandler();
 
-  let m1 = new Map("map1", "engine/map/map1.txt");
+  let m1 = new Map("m1", "engine/map/m1/m1.txt");
   map_handler.add(m1);
   map_handler.active_map = m1;
 
