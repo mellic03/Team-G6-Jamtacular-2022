fc63dac6cd655f8d53a39e8ec879b2633b0343d0
Aiden-Larson <qaznob@gmail.com>
2022-11-10 15:06:11 +1000

    map 2 mostly done

--- engine/map/mapinit.js
+++ engine/map/mapinit.js
@@ -5,7 +5,7 @@ function map_init() {
   let m1 = new Map("m1", "engine/map/maps/m1.txt");
   map_handler.add(m1);
   
-  map_handler.add(new Map("m1", "engine/map/maps/m2.txt"));
+  map_handler.add(new Map("m2", "engine/map/maps/m2.txt"));
   map_handler.add(new Map("m3", "engine/map/maps/m3.txt"));
   map_handler.add(new Map("m4", "engine/map/maps/m4.txt"));
   // map_handler.add(new Map("m5", "engine/map/maps/m5.txt"));
