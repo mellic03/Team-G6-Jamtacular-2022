b01253b5ca0b4796043e6a0312d81e6c88fe23ad
Michael Ellicott <mellic03@gmail.com>
2022-11-11 14:06:28 +1000

    map audio transitions

--- engine/audio/audiohandler.js
+++ engine/audio/audiohandler.js
@@ -8,16 +8,13 @@ class AudioHandler {
   }
 
   preload() {
-
     /* object properties can be created by accessing them for the first time
       this.tracks.map1_track1 = loadSound() blah blah blah I cant remember how sound works
     */
-
     this.tracks.m1 = loadSound('engine/audio/sounds/music/track1.mp3');
     this.tracks.m2 = loadSound('engine/audio/sounds/music/track2.mp3');
     this.tracks.m3 = loadSound('engine/audio/sounds/music/track3.mp3');
     this.tracks.m4 = loadSound('engine/audio/sounds/music/track4.mp3');
-
   }
 
   setup() {
